d8356f3798c644d250aa60f1bb45757519d238a9
Chozharajan_V ( Full Stack Devp) <50139220+chozhaV@users.noreply.github.com>
2025-07-29 10:44:24 +0530

    Add .next/static/chunks/src_app_page_tsx_9ab15317._.js 1753766064004

---
+++ .next/static/chunks/src_app_page_tsx_9ab15317._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_65e20883._.js",
+  "static/chunks/src_d3598cc3._.js"
+],
+    source: "dynamic"
+});
